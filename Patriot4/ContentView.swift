360ec05595f2944eca7833dd719085bfa6a7e83b
Ron <ron@lisles.net>
2022-08-22 05:59:08 -0400

    initial files

---
+++ Patriot4/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Patriot4
+//
+//  Created by Ron Lisle on 8/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Patriot4/Patriot4App.swift
@@ -0,0 +1,17 @@
+//
+//  Patriot4App.swift
+//  Patriot4
+//
+//  Created by Ron Lisle on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Patriot4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
